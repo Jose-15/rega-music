45ffeb0917f9f7f1db8fbce55d26132ec6bb17c5
Jose-15 <73955332+Jose-15@users.noreply.github.com>
2022-11-25 11:01:05 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>REGA-MUSIC</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/materialize.min.css">  
+  <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/new.css">
+  <link rel="stylesheet" href="assets/css/index.css">
+  <link rel="stylesheet" href="assets/css/galeria.css">
+  <link rel="stylesheet" href="assets/css/contacto.css">
+  <link rel="stylesheet" href="assets/css/footer.css">
+  <!---PWA-->
+  <meta charset="utf-8">
+  <meta name="theme-color" content="#F0DB4F">
+  <meta name="MobileOptimized" content="width">
+  <meta name="HandheldFriendly" content="true">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <link rel="apple-touch-startup-image" href="assets/img/Logo.png">
+  <link rel="apple-touch-icon" href="assets/img/Logo.png">
+  <link rel="manifest" href="./manifest.webmanifest">
+  <script></script>
+</head>
+<body>
+<?php
+        @include("view/plantillas/na.php")
+    ?>
+
+  <main class="main-info" id="inicio">
+    <div class="container">
+      <div data-aos="fade-right" data-aos-duration="2000">
+        <h1>Nuestra Historia</h1>
+        <p style="font-weight: normal; text-align: justify;">La idea surge en una tienda de venta de guitarras viendo la necesidad y petición de los clientes que buscaban instrumentos musicales en la zona centro de Monterrey, introduciendo una pequeña variedad de guitarras y accesorios que progresivamente fue incrementando su venta, impulsando a crear Rega Music, una tienda especializada y con una gran variedad de producto demandado por el mercado regiomontano</p>
+      </div>
+       <img src="assets/img/logo1.PNG" alt="xk-drums tama" data-aos="fade-left" data-aos-duration="2000">
+    </div>
+  </main>
+
+  <section id="nosotros">
+    <div class="container">
+        <div class="img-container"></div>
+        <div class="texto">
+
+            <h2>Misión</h2>
+            <p style="font-weight: normal; text-align: justify;">Es brindar a nuestros clientes una atención amable, acompañada de una pronta respuesta a sus requisiciones, necesidades o dudas sobre cualquier instrumento o sistema de audio solicitado, generándoles la confianza de que todos nuestros productos vendrán acompañados de una excelente calidad, de una inmediata garantía, y de un valor agregado que el cliente identificara al realizar una compra con nosotros, además de poner a su disposición una gran gama de productos que se adecuaran a cualquier presupuesto.</p>
+            <h2>Visión</h2>
+            <p style="font-weight: normal; text-align: justify;">Ser la empresa líder en el mercado, identificada como una empresa seria por el alto nivel de compromiso de los vendedores a las necesidades de los clientes, con una sorprendente rapidez en el surtimiento de pedidos y de una muy amable atención al cliente, distinguida por su siempre constante innovación en nuevas marcas para brindar mas opciones a nuestros clientes, así como por ser proveedores de servicios de post venta para dar un valor agregado a la compra de cualquier de nuestros productos.</p>
+            
+        </div>
+    </div>
+</section>
+
+  <article class="products" data-aos="fade-up" data-aos-duration="3000">
+    <section class="product-title" id="products">
+      <h2>Productos</h2>
+      <div class="divider"></div>
+    </section>
+    <div class="carousel">
+      <a class="carousel-item" href="#one!" alt="Sonor Drum set">
+        <img src="assets/img/4.png">
+        <h5>Pianos</h5>
+      </a>
+      <a class="carousel-item" href="#two!">
+        <img src="assets/img/5.png" alt="Roland drum set">
+        <h5>Guitarras</h5>
+      </a>
+      <a class="carousel-item" href="#three!">
+        <img src="assets/img/7.png" alt="Roland drum set">
+        <h5>Baterias</h5>
+      </a>
+      <a class="carousel-item" href="#four!">
+        <img src="assets/img/1.png" alt="Sjc ">
+        <h5>Guitarra</h5>
+      </a>
+      <a class="carousel-item" href="#five!">
+        <img src="assets/img/2.png" alt="Lummier">
+        <h5>Acordeon</h5>
+      </a>
+    </div>
+  </article>
+  
+
+  
+  <svg xmlns="http://www.w3.org/2000/svg" class="skew" viewBox="0 0 1440 320"></svg>
+
+  <center><section id="galeria" style="background-color: black; color: white; text-align: center;">
+    
+    <br>
+    <h2>Galeria</h2>
+    <div class="grid-container">
+        <div
+          class="grid-item tall"
+          style="background-image: url(assets/img/20221029043821.jpg)"
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029050420.jpg);
+          "
+        ></div>
+  
+        <div
+          class="grid-item wide"
+          style="
+            background-image: url(assets/img/20221029043935.jpg);
+          "
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029052409.jpg);
+          "
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029052604.jpg);
+          "
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029053555.jpg);
+          "
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029053706.jpg);
+          "
+        ></div>
+        <div
+          class="grid-item"
+          style="background-image: url(assets/img/20221029054711.jpg)"
+        ></div>
+        <div
+          class="grid-item tall wide"
+          style="
+            background-image: url(assets/img/20221029055514.jpg);
+          "
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029051957.jpg);
+          "
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029060844.jpg);
+          "
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029045603.jpg);"
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029045721.jpg);"
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029060339.jpg);"
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221029052234.jpg);"
+        ></div>
+        <div
+          class="grid-item"
+          style="
+            background-image: url(assets/img/20221110171022.jpg);"
+        ></div>
+</section></center>
+
+  <script src="//code.tidio.co/8o5ioep2jseyrtyicggsjbehjmtqxvmq.js" async></script>
+  
+    
+     <section id="politicas">
+         <div class="container">
+           <ul>
+           <center><h1>Politicas</h1></center>
+             <li>1.- Responsable de la protección de sus Datos Personales. </li>
+             <li style="font-weight: normal; text-align: justify;">EASY MUSIC REGA, S.A. de C.V. es el responsable de recabar, tratar y proteger sus datos personales. Al hacer uso de los servicios ofrecidos por EASY MUSIC REGA, se entenderá que reconoce y acepta los términos establecidos en el presente Aviso de Privacidad, sin perjuicio de que posteriormente usted decidiera ejercer sus derechos de revocación u oposición.</li>
+             <li>2.- Definiciones</li>
+             <li style="font-weight: normal; text-align: justify;">EASY MUSIC REGA le brinda un listado de las definiciones para tener mayor claridad sobre el contenido del aviso de privacidad, Fragmentos de información de una página web que se almacena en el disco duro del dispositivo utilizado para conectarse a Internet, a través de su navegador a petición del servidor de la página. Dirección IP: Es el identificador lógico numérico del dispositivo utilizado para conectarse dentro de Internet, asignado por el proveedor de servicios de Internet al momento en que un usuario se conecta.</li>
+             <li>3.- Datos Personales recabados para su tratamiento y los medios de obtención</li>
+             <li style="font-weight: normal; text-align: justify;">Los Datos Personales que usted nos proporciona, serán tratados de manera lícita y únicamente para las finalidades descritas en el presente aviso de privacidad. Asimismo, para la protección de sus Datos Personales estableceremos las medidas de seguridad administrativas, técnicas y físicas que nos permitan proteger sus datos personales contra daño, pérdida, alteración, destrucción o un uso distinto al señalado en el presente aviso de privacidad.</li>
+           </ul>      
+         </div>
+     </section>
+ 
+     <section id="contacto">
+    <?php
+        @include("view/auth/contacto.php")
+    ?>
+    </section>
+     
+     <br><br><br>
+      <center><?php @include("view/plantillas/footer.php") ?></center>
+
+      <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+      <script src="assets/js/materialize.min.js"></script>
+      <script src="assets/js/new.js"></script>
+      <script>
+        var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+      </script>
+      <script src="js.js"></script>
+</body>
+</html>
